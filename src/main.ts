98c10c261f37050fba3e7ec5e2ae82722f6b7ca1
sajeebro <sajithmadusanka720@gmail.com>
2025-04-22 01:37:41 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
+  // Load environment variables
+  void ConfigModule.forRoot({ isGlobal: true });
+  const host = process.env.HOST || 'localhost';
+  const port = process.env.PORT || '50054';
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.KAFKA,
       options: {
         client: {
-          brokers: ['localhost:9092'],
+          brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
         },
         consumer: {
           groupId: 'notification-consumer',
@@ -19,6 +24,6 @@ async function bootstrap() {
   );
   app.enableShutdownHooks();
   await app.listen();
-  console.log('Notification service is running on: http://localhost:50054');
+  console.log(`Notification service is running on: http://${host}:${port}`);
 }
 void bootstrap();
